bdb13980eea94f5a6812c044f45e8d29a2d7261c
Lucas Sartori <lucas.sartm@gmail.com>
2019-03-20 19:52:29 -0300

    Updates

---
+++ precache-manifest.007f42f87f443ab29b1fad78f3d13ed2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1d3f7114f5b5dc13672",
+    "url": "/why-slack/static/js/runtime~main.f1d3f711.js"
+  },
+  {
+    "revision": "b6a3f95a9b9b2067906b",
+    "url": "/why-slack/static/js/main.b6a3f95a.chunk.js"
+  },
+  {
+    "revision": "4c7c88b792bf71c32e8b",
+    "url": "/why-slack/static/js/1.4c7c88b7.chunk.js"
+  },
+  {
+    "revision": "4c7c88b792bf71c32e8b",
+    "url": "/why-slack/static/css/1.cabc8b80.chunk.css"
+  },
+  {
+    "revision": "b84ad92ede5df63e712381291e53b329",
+    "url": "/why-slack/index.html"
+  }
+];
