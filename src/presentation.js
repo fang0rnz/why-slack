8911949d14cb57679c41eec3ebeeef96681f017f
Lucas Sartori <lucas.sartm@gmail.com>
2019-03-20 19:48:09 -0300

    finished

--- src/presentation.js
+++ src/presentation.js
@@ -2,20 +2,7 @@
 import React from 'react';
 
 // Import Spectacle Core tags
-import {
-  BlockQuote,
-  Cite,
-  Deck,
-  Heading,
-  ListItem,
-  List,
-  Quote,
-  Slide,
-  Text,
-  Notes,
-  Appear,
-  Image
-} from 'spectacle';
+import { Deck, Heading, Slide, Text, Notes, Appear, Image } from 'spectacle';
 
 // Import theme
 import createTheme from 'spectacle/lib/themes/default';
@@ -51,21 +38,23 @@ export default class Presentation extends React.Component {
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
             PRA CONVENCER
           </Heading>
-
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
             QUE CENTRALIZAR A COMUNICAÇÃO NO SLACK NÃO É UMA IDEIA TERRÍVEL
           </Text>
         </Slide>
         <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <Heading>POR QUE?</Heading>
+          <Heading>POR QUÊ?</Heading>
         </Slide>
         <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={1} caps lineHeight={1} textColor="secondary">
+            🤔
+          </Heading>
           <Appear>
-            <li>O hangouts é limitado</li>
+            <li>O hangouts é limitado e será descontinuado em breve</li>
           </Appear>
           <Appear>
             <li style={{ paddingTop: 50 }}>
-              Whatasapp é ruim, não é a ferramenta ideal para comunicação de um
+              Whatsapp é ruim, não é a ferramenta ideal para comunicação de um
               time de tecnologia de alta performance
             </li>
           </Appear>
@@ -75,127 +64,27 @@ export default class Presentation extends React.Component {
               tecnologia
             </li>
           </Appear>
+          <Appear>
+            <li style={{ paddingTop: 50 }}>
+              A comunicação está espalhada por vários meios
+            </li>
+          </Appear>
         </Slide>
         <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Notes>
-            <li>Contar história de como fui rejeitado</li>
-            <li>
-              O mercado antigo nunca se preparou para a profissão de programador
-            </li>
-            <li>
-              Mão de obra qualificada NECESSITAVA de ensino formal por boas
-              razões
-            </li>
-            <li> O cenário mudou, visto que o conhecimento é acessível</li>
-            <li> A maioria dos métodos de seleção está defasado</li>
-            <li> Isso impacta diretamente no financeito de qualquer empresa</li>
-            <li>
-              Caixa de skinner de devs para resolverem um problema, o que leva a
-              um problema
-            </li>
-          </Notes>
-
-          <Heading textColor="secondary" caps>
-            👩🏿‍🏭 🏭 👨‍🏭
-          </Heading>
+          <Image width="75%" src="slack.png" />
         </Slide>
-
-        <Slide bgColor="secondary" transition={['slide']}>
-          <Image width="50%" src="dan.jpg" />
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+          <Image width="75%" src="slack1.png" />
         </Slide>
-        <Slide transition={['slide']} bgColor="primary" textColor="tertiary">
-          <Heading style={{ marginTop: '50px' }}>👩💻 👨‍💻</Heading>
-          <Appear>
-            <Heading style={{ marginTop: '50px' }}>☕</Heading>
-          </Appear>
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+          <Image width="75%" src="slack2.png" />
         </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <Notes>
-            <li>Resultado de se ver gente com 10 anos de experiência</li>
-            <li>Não se sentir suficiente</li>
-            <li>Medo de tentar</li>
-          </Notes>
-          <Heading size={4} textColor="primary">
-            Síndrome do impostor 😞
-          </Heading>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <Notes>
-            <li> Paciência</li>
-            <li> Estrutura de pair programming</li>
-            <li>
-              Nem sempre a pessoa mais experiente é a mais apta a ajudar um
-              desenvolvedor junior
-            </li>
-          </Notes>
-          <Image width="100%" src="lotr.gif" />
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <Notes>
-            <li> Paciência</li>
-            <li> Estrutura de pair programming</li>
-            <li>
-              Nem sempre a pessoa mais experiente é a mais apta a ajudar um
-              desenvolvedor junior
-            </li>
-          </Notes>
-          <Image width="100%" src="smith.gif" />
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <Heading size={1}>💭</Heading>
-          <Appear>
-            <li style={{ paddingTop: '60px' }}>Estratégias de ensino</li>
-          </Appear>
-          <Appear>
-            <li>
-              Cultura de tutoria - é ok e esperado não entender algo de primeira
-            </li>
-          </Appear>
-          <Appear>
-            <li>Escolha de tarefas apropriadas para o nível de conhecimento</li>
-          </Appear>
-          <Appear>
-            <li>Processo mais visual </li>
-          </Appear>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <Image width="100%" src="kungfu.gif" />
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <Heading>Desafio para o coffee break</Heading>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <Heading size={5}>💡</Heading>
-          <Appear>
-            <li>
-              <strong>Compartilhe</strong> experiências de tutoria
-            </li>
-          </Appear>
-          <Appear>
-            <li>
-              <strong>Discuta</strong> como você e seu time podem rever
-              processos defasados
-            </li>
-          </Appear>
-          <Appear>
-            <li>
-              <strong>Ache</strong> alguém com vontade de aprender / entrar na
-              indústria e tente ajudar.{' '}
-              <em>Especialmente se você for júnior</em>
-            </li>
-          </Appear>
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+          <Image width="100%" src="slack4.png" />
         </Slide>
 
         <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <Image width="100%" src="junior.gif" />
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <Heading>Obrigado! 🎉</Heading>
-          <div style={{ marginTop: '50px' }}>
-            <div>
-              <span>@fang0rnz</span>
-            </div>
-          </div>
+          <Heading>Hands on!</Heading>
         </Slide>
       </Deck>
     );
